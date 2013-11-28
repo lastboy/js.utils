81bcaa7169d0454a9fb73f4dfe7f49629917529a
Levin, Arik <arik.levine@hp.com>
2013-11-28 19:01:12 +0200

    XML module added to the main js.util service

--- jsutils.js
+++ jsutils.js
@@ -9,6 +9,7 @@ module.exports = function() {
         Array: require("./utils/Array.js"),
         Template: require("./utils/Template.js"),
         NPM: require("./utils/NPM.js"),
+        XML: require("./utils/XML.js"),
 
         init: function(config) {
             if (config) {
