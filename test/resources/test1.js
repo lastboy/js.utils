7d2f971ec5e6ef185d6bd81d1a2d0ed06dfa05fc
arik <lewin.arik@gmail.com>
2013-12-18 18:03:25 +0200

    test resources added for Task tests

---
+++ test/resources/test1.js
@@ -0,0 +1,17 @@
+function test() {
+    // this content is to be minified
+
+    this.x1 = 1;
+    this.y1 = 1;
+
+
+    this.getX1 = function() {
+        return this.x1;
+    };
+
+    this.getY1 = function() {
+        return this.y1;
+    };
+
+
+}
